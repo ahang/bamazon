d2fc380768ce16a95921b4d432a7368c88689a04
dashaman <hang.andy.t@gmail.com>
2017-04-18 10:13:29 -0500

    missing one column in schema

--- schema.sql
+++ schema.sql
@@ -3,11 +3,12 @@ CREATE DATABASE Bamazon;
 USE Bamazon;
 
 CREATE TABLE products (
-	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100),
-	price DECIMAL(10,4),
+    price DECIMAL(10,4),
     stock_quantity INTEGER(11),
+    product_sales INTEGER(11),
     PRIMARY KEY (item_id)
 );
 
