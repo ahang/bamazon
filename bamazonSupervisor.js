654b1b934286abf541454980687caf1acb4dd868
dashaman <hang.andy.t@gmail.com>
2017-04-18 23:38:31 -0500

    got rid of pws

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "m5J5V77YhJgu",
+    password: "#",
     database: "Bamazon"
 });
 //======================================================
